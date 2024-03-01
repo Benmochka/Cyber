46be8ab3ff24074790c7fb64f0db67aeb5adc1fd
Benmochka <benjamin.vaniche@gmail.com>
2024-03-01 13:00:17 +0200

    Simple thread code

--- Start.py
+++ Start.py
@@ -1,1 +1,20 @@
-print("Hello world")
+from threading import Thread
+
+counter = 0
+def add():
+    global counter
+    for _ in range(1000000):
+        counter += 1
+    
+def decrement():
+    global counter
+    for _ in range(1000000):
+        counter -= 1    
+
+thread1 = Thread(target = add)  
+thread2 = Thread(target = decrement)
+thread1.start()
+thread2.start()    
+thread1.join() 
+thread2.join()
+print(counter)
