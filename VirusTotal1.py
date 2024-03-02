d0b67734329d9ebb9d4c98e30b818a1f9a14ad80
Benmochka <benjamin.vaniche@gmail.com>
2024-03-02 12:58:36 +0200

    Almost finished the gui

--- VirusTotal1.py
+++ VirusTotal1.py
@@ -19,13 +19,16 @@ def scanFileAtVT(file_path):
     data = response1.json()
 
     ##printing how many sources found it malicious
-    countpos = 0
-    counttotal = 0
-    for i in data['scans']:
-        counttotal += 1
-        if(data['scans'][i]['detected'] == True):
-            countpos += 1
-    print(f'{countpos} sources out of {counttotal} found ' + os.path.basename(file_path) + ' malicious')        
+    try:
+        countpos = 0
+        counttotal = 0
+        for i in data['scans']:
+            counttotal += 1
+            if(data['scans'][i]['detected'] == True):
+                countpos += 1
+        print(f'{countpos} sources out of {counttotal} found ' + os.path.basename(file_path) + ' malicious')  
+    except KeyError:
+        print("The file couldn't be scanned")          
 
 def getFiles(path):
     for filename in os.listdir(path):
@@ -36,14 +39,24 @@ def getFiles(path):
         else:
             scanFileAtVT(fullname) 
 
+
 import tkinter as tk
 from tkinter import *
 w = tk.Tk()
 w.title('Antivirus - Virus Total API')
-Label(w, text='Enter your file/folder you want to check').grid(row=1)
-e1 = Entry(w)
+Label(w, text='Enter your file/folder you want to check(close the brackets at the end)').grid(row=1)
+e1 = Entry(w, width = 70)
 e1.grid(row=1, column=1)
+e1.insert(0, 'r"')
 e2 = Label(w, text='Antivirus made by Benjamin Vaniche, Enjoy!')
-e2.grid(row=10, column=10)
+e2.grid(row=10, column=3)
+def AtClick():
+    if os.path.isdir(e1.get()):
+        Label(w , text = getFiles(e1.get())).grid(row = 2)
+    if os.path.isfile(e1.get()):
+        Label(w , text = scanFileAtVT(e1.get())).grid(row = 2)
+button = Button(w, text = 'Scan the file', command = AtClick)
+button.grid(row = 1, column = 2)
+
 w.mainloop()
 
