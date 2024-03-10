cffba41e2345c507068d464ee18869b81cce8b7e
Benmochka <benjamin.vaniche@gmail.com>
2024-03-10 09:05:33 +0200

    changed the gui to gradio

--- VirusTotal1.py
+++ VirusTotal1.py
@@ -40,23 +40,37 @@ def getFiles(path):
             scanFileAtVT(fullname) 
 
 
-import tkinter as tk
-from tkinter import *
-w = tk.Tk()
-w.title('Antivirus - Virus Total API')
-Label(w, text='Enter your file/folder you want to check(close the brackets at the end)').grid(row=1)
-e1 = Entry(w, width = 70)
-e1.grid(row=1, column=1)
-e1.insert(0, 'r"')
-e2 = Label(w, text='Antivirus made by Benjamin Vaniche, Enjoy!')
-e2.grid(row=10, column=3)
-def AtClick():
-    if os.path.isdir(e1.get()):
-        Label(w , text = getFiles(e1.get())).grid(row = 2)
-    if os.path.isfile(e1.get()):
-        Label(w , text = scanFileAtVT(e1.get())).grid(row = 2)
-button = Button(w, text = 'Scan the file', command = AtClick)
-button.grid(row = 1, column = 2)
+# import tkinter as tk
+# from tkinter import *
+# w = tk.Tk()
+# w.title('Antivirus - Virus Total API')
+# Label(w, text='Enter your file/folder you want to check(close the brackets at the end)').grid(row=1)
+# e1 = Entry(w, width = 70)
+# e1.grid(row=1, column=1)
+# e1.insert(0, 'r"')
+# e2 = Label(w, text='Antivirus made by Benjamin Vaniche, Enjoy!')
+# e2.grid(row=10, column=3)
+# def AtClick():
+#     if os.path.isdir(e1.get()):
+#         Label(w , text = getFiles(e1.get())).grid(row = 2)
+#     if os.path.isfile(e1.get()):
+#         Label(w , text = scanFileAtVT(e1.get())).grid(row = 2)
+# button = Button(w, text = 'Scan the file', command = AtClick)
+# button.grid(row = 1, column = 2)
+# w.mainloop()
+            
+import gradio as gr
+def dir_or_file(path):
+    if os.path.isfile(path):
+        scanFileAtVT(path)
+    if os.path.isdir(path):
+        getFiles(path)
 
-w.mainloop()
+demo = gr.Interface(
+    fn = dir_or_file,
+    inputs = gr.Textbox(lines = 2, placeholder = 'Link here(with r" before and close the brackets at the end)'),
+    outputs = 'text',
+)
+
+demo.launch(share = True)
 
