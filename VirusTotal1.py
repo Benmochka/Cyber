122fc7722d76fe6ef842a648f109948e2832538e
Benmochka <benjamin.vaniche@gmail.com>
2024-03-11 08:52:02 +0200

    Improved the gui, and it works perfectly

--- VirusTotal1.py
+++ VirusTotal1.py
@@ -26,9 +26,9 @@ def scanFileAtVT(file_path):
             counttotal += 1
             if(data['scans'][i]['detected'] == True):
                 countpos += 1
-        print(f'{countpos} sources out of {counttotal} found ' + os.path.basename(file_path) + ' malicious')  
+        return f'{countpos} sources out of {counttotal} found ' + os.path.basename(file_path) + ' malicious'
     except KeyError:
-        print("The file couldn't be scanned")          
+        return "The file couldn't be scanned"     
 
 def getFiles(path):
     for filename in os.listdir(path):
@@ -40,37 +40,46 @@ def getFiles(path):
             scanFileAtVT(fullname) 
 
 
-# import tkinter as tk
-# from tkinter import *
-# w = tk.Tk()
-# w.title('Antivirus - Virus Total API')
-# Label(w, text='Enter your file/folder you want to check(close the brackets at the end)').grid(row=1)
-# e1 = Entry(w, width = 70)
-# e1.grid(row=1, column=1)
-# e1.insert(0, 'r"')
-# e2 = Label(w, text='Antivirus made by Benjamin Vaniche, Enjoy!')
-# e2.grid(row=10, column=3)
-# def AtClick():
-#     if os.path.isdir(e1.get()):
-#         Label(w , text = getFiles(e1.get())).grid(row = 2)
-#     if os.path.isfile(e1.get()):
-#         Label(w , text = scanFileAtVT(e1.get())).grid(row = 2)
-# button = Button(w, text = 'Scan the file', command = AtClick)
-# button.grid(row = 1, column = 2)
-# w.mainloop()
+import tkinter as tk
+from tkinter import filedialog
+import os
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
+
+def select_file():
+    file_path = filedialog.askopenfilename()
+    return file_path
+def AtClick():
+    file_path = file_path_var.get()
+    if file_path:
+        if os.path.isdir(file_path):
+            result = getFiles(file_path)
+            result = str(result) 
+            result_text.delete('1.0', tk.END)  
+            result_text.insert(tk.END, result)
+        elif os.path.isfile(file_path):
+            result = scanFileAtVT(file_path)
+            result = str(result)  # Convert the result to a string
+            result_text.delete('1.0', tk.END)  # Clear previous result
+            result_text.insert(tk.END, result)
+
+w = tk.Tk()
+w.title('Antivirus - Virus Total API')
+
+e2 = tk.Label(w, text='Antivirus made by Benjamin Vaniche, Enjoy!')
+e2.grid(row=0, column=0, columnspan=2)
+
+file_path_var = tk.StringVar()
+
+button1 = ttk.Button(w, text='Select a file', command=lambda: file_path_var.set(select_file()))
+button1.grid(row=1, column=0)
+
+button2 = ttk.Button(w, text='Scan the file', command=AtClick)
+button2.grid(row=1, column=1)
+
+result_text = tk.Text(w, height=10, width=50)
+result_text.grid(row=2, column=0, columnspan=3, padx=10, pady=10)
+
+w.mainloop()
             
-import gradio as gr
-def dir_or_file(path):
-    if os.path.isfile(path):
-        scanFileAtVT(path)
-    if os.path.isdir(path):
-        getFiles(path)
-
-demo = gr.Interface(
-    fn = dir_or_file,
-    inputs = gr.Textbox(lines = 2, placeholder = 'Link here(with r" before and close the brackets at the end)'),
-    outputs = 'text',
-)
-
-demo.launch(share = True)
 
