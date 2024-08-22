cc5eeb87c2a585af5623dcfd95615445c1a752ae
Benmochka <benjamin.vaniche@gmail.com>
2024-08-22 13:16:56 +0300

    finished most of the classes

--- Client_Take_Control_CP.py
+++ Client_Take_Control_CP.py
@@ -3,30 +3,17 @@ import threading
 import keyboard
 import mouse
 import pyautogui
-import time
 import io
+import time
 
-size = 1024
-form = "utf-8" 
-server_ip = "127.0.0.1"
-keyboard_port = 5000
-mouse_port = 5001
-screen_port = 5002
-encode = 'utf-8'
-
-class Networks:
-    def __init__(self) -> None:
-        pass
-
-
-class Keyboard:
+class KeyboardClient:
     def __init__(self, server_ip, keyboard_port) -> None:
         self.server_ip = server_ip
         self.keyboard_port = keyboard_port
-        self.tcp_kb_client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.tcp_kb_client.connect(self.server_ip, self.keyboard_port)
+        self.tcp_kb_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.tcp_kb_client.connect((self.server_ip, self.keyboard_port))
 
-    def press_key(event):
+    def press_key(self, event):
         keyboard.press_and_release(event)
 
     def start_receiving(self):
@@ -38,9 +25,7 @@ class Keyboard:
             self.press_key(event)
         self.tcp_kb_client.close()
 
-
-# Create an instance of the KeyboardClient class
-keyboard_client = Keyboard(server_ip, keyboard_port)
+keyboard_client = KeyboardClient('127.0.0.1', 5000)
 client_thread = threading.Thread(target=keyboard_client.start_receiving)
 client_thread.start()
 
@@ -48,27 +33,49 @@ try:
     client_thread.join()
 except KeyboardInterrupt:
     print("Terminating...")
-    keyboard_client.tcp_kb_client.close() 
+    keyboard_client.tcp_kb_client.close()
 
-class Mouse:
+class MouseClient:
     def __init__(self, server_ip, mouse_port) -> None:
         self.server_ip = server_ip
         self.mouse_port = mouse_port
-        self.tcp_ms_client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.tcp_ms_client.connect(self.server_ip, self.mouse_port)
-    
-    def moveAndClick(click_pos,kindofclick):
-        mouse.move(click_pos[0], click_pos[1],absolute=True,duration = 0.2)
+        self.tcp_ms_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.tcp_ms_client.connect((self.server_ip, self.mouse_port))
+
+    def move_and_click(self, click_pos, kindofclick):
+        mouse.move(click_pos[0], click_pos[1], absolute=True, duration=0.2)
         if kindofclick == "left":
             mouse.click('left')
         if kindofclick == "right":
-            mouse.click('right')  
+            mouse.click('right')
 
-    def moveAndScroll(scroll_pos,scrollDelta):
-        mouse.move(scroll_pos[0], scroll_pos[1],absolute=True,duration=0.2)
+    def move_and_scroll(self, scroll_pos, scrollDelta):
+        mouse.move(scroll_pos[0], scroll_pos[1], absolute=True, duration=0.2)
         mouse.wheel(scrollDelta)
 
-class Screenshots:
+    def start_receiving(self):
+        while True:
+            data = self.tcp_ms_client.recv(1024)
+            if not data:
+                break
+            parts = data.decode('utf-8').split(',')
+            if parts[0] == 'click':
+                self.move_and_click((int(parts[1]), int(parts[2])), parts[3])
+            elif parts[0] == 'scroll':
+                self.move_and_scroll((int(parts[1]), int(parts[2])), int(parts[3]))
+        self.tcp_ms_client.close()
+
+mouse_client = MouseClient('127.0.0.1', 5001)
+mouse_thread = threading.Thread(target=mouse_client.start_receiving)
+mouse_thread.start()
+
+try:
+    mouse_thread.join()
+except KeyboardInterrupt:
+    print("Terminating...")
+    mouse_client.tcp_ms_client.close()
+
+class ScreenshotsClient:
     def __init__(self, server_ip, port):
         self.server_ip = server_ip
         self.port = port
@@ -82,8 +89,12 @@ class Screenshots:
             self.udp_socket.sendto(byte_arr.getvalue(), (self.server_ip, self.port))
             time.sleep(1)
 
-screenshots_sender = Screenshots(server_ip, screen_port)
-screenshots_thread = threading.Thread(target = screenshots_sender.take_and_send())
+screenshots_sender = ScreenshotsClient('127.0.0.1', 5002)
+screenshots_thread = threading.Thread(target=screenshots_sender.take_and_send)
 screenshots_thread.start()
 
-screenshots_thread.join()
+try:
+    screenshots_thread.join()
+except KeyboardInterrupt:
+    print("Terminating...")
+    screenshots_sender.udp_socket.close()
--- Server_Take_Control_CP.py
+++ Server_Take_Control_CP.py
@@ -3,18 +3,17 @@ import threading
 import keyboard
 import mouse
 from PIL import Image
-import io 
+import io
 
 size = 1024
-form = "utf-8" 
+form = "utf-8"
 server_ip = "127.0.0.1"
 keyboard_port = 5000
 mouse_port = 5001
 screen_port = 5002
 encode = 'utf-8'
 
-
-class Keyboard:
+class KeyboardServer:
     def __init__(self, server_ip, keyboard_port) -> None:
         self.server_ip = server_ip
         self.keyboard_port = keyboard_port
@@ -23,7 +22,7 @@ class Keyboard:
         self.tcp_kb_socket.listen(1)
         self.client_socket, self.client_address = self.tcp_kb_socket.accept()
 
-    def listen_to_click(self,event):
+    def listen_to_click(self, event):
         if event.event_type == keyboard.KEY_DOWN:
             self.client_socket.sendall(event.name.encode(encode))
 
@@ -31,10 +30,8 @@ class Keyboard:
         keyboard.hook(self.listen_to_click)
         keyboard.wait()
 
-
-
-keyboard_listener = Keyboard(server_ip, keyboard_port)
-keyboard_thread = threading.Thread(target = keyboard_listener.start_listening)
+keyboard_listener = KeyboardServer(server_ip, keyboard_port)
+keyboard_thread = threading.Thread(target=keyboard_listener.start_listening)
 keyboard_thread.start()
 
 try:
@@ -44,56 +41,54 @@ except KeyboardInterrupt:
     keyboard.unhook_all()
     keyboard_listener.client_socket.close()
     keyboard_listener.tcp_kb_socket.close()
-    
-class Mouse:
+
+class MouseServer:
     def __init__(self, server_ip, mouse_port) -> None:
         self.server_ip = server_ip
         self.mouse_port = mouse_port
-        self.tcp_ms_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.tcp_ms_socket.bind(server_ip, mouse_port)
+        self.tcp_ms_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.tcp_ms_socket.bind((self.server_ip, self.mouse_port))
         self.tcp_ms_socket.listen(1)
-        self.client_socket, self_client_address = self.tcp_ms_socket.accept()
-        self.kindOfClick = ""
-    
+        self.client_socket, self.client_address = self.tcp_ms_socket.accept()
+
     def on_click(self, event):
-        if event.event_type == mouse.UP and event.button == mouse.LEFT:
-            self.client_socket.sendall(event.x.encode(encode),event.y.encode(encode))
-            kindOfClick = "left"
+        if event.event_type == mouse.is_pressed("right"):
+            position = mouse.get_position()
+            click = "right"
+            self.client_socket.sendall(click.encode(encode))
+            self.client_socket.sendall(position.encode(encode))
+        if event.event_type == mouse.is_pressed("left"):
+            position = mouse.get_position()
+            click = "left"
+            self.client_socket.sendall(click.encode(encode))
+            self.client_socket.sendall(position.encode(encode))
 
-            self.client_socket.sendall(kindOfClick.encode(encode))
-        elif event.event_type == mouse.UP and event.button == mouse.RIGHT:
-            self.client_socket.sendall(event.x.encode(encode),event.y.encode(encode))
-            kindOfClick = "right"
-            self.client_socket.sendall(kindOfClick.encode(encode))   
-       
-    def scroll_wheel(self, event):
-        if event.event_type == mouse.wheel:
-            self.client_socket.sendall(event.x.encode(encode),event.y.encode(encode))
-            self.client_socket.sendall(event.delta.encode(encode))
-    
+    def on_scroll(self, event):
+        if event.event_type == mouse.scroll
+        self.client_socket.sendall(message.encode(encode))
+
     def start_listening(self):
-        mouse.hook(self.on_click)
-        mouse.hook(self.scroll_wheel)
-        mouse.wait
+        mouse_listener = mouse.Listener(on_click=self.on_click, on_scroll=self.on_scroll)
+        mouse_listener.start()
+        mouse_listener.join()
 
-mouse_listener = Mouse(server_ip, mouse_port)      
-mouse_thread = threading.Thread(target = mouse_listener.start_listening())
+mouse_listener = MouseServer(server_ip, mouse_port)
+mouse_thread = threading.Thread(target=mouse_listener.start_listening)
 mouse_thread.start()
 
 try:
     mouse_thread.join()
 except KeyboardInterrupt:
     print("Terminating...")
-    mouse.unhook_all()
     mouse_listener.client_socket.close()
     mouse_listener.tcp_ms_socket.close()
 
-class Screenshots:
+class ScreenshotsServer:
     def __init__(self, server_ip, port):
         self.port = port
         self.server_ip = server_ip
         self.udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.udp_socket.connect((self.server_ip, self.port))
+        self.udp_socket.bind((self.server_ip, self.port))
 
     def recv_and_show(self):
         while True:
@@ -102,4 +97,12 @@ class Screenshots:
             image = Image.open(byte_arr)
             image.show()
 
+screenshots_receiver = ScreenshotsServer(server_ip, screen_port)
+screenshots_thread = threading.Thread(target=screenshots_receiver.recv_and_show)
+screenshots_thread.start()
 
+try:
+    screenshots_thread.join()
+except KeyboardInterrupt:
+    print("Terminating...")
+    screenshots_receiver.udp_socket.close()
